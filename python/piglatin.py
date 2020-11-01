4e233742ff537ce83566d2b3b0c58b1fb2791436
Mueez Khan <30333942+M3KO@users.noreply.github.com>
2020-11-01 04:17:13 -0500

    Pig Latin conversion for a sentence

---
+++ python/piglatin.py
@@ -0,0 +1,13 @@
+def pigLatin(words): # Takes in a word to convert to pig latin
+    vowels = ("A", "E", "I", "O", "U", "a", "e", "i", "o", "u") # Identify all vowels with and without capitalization
+    for letter in words: # Checks each letter in a word
+        if letter in vowels: # Checks for the first vowel in the word
+            words = words + "ay" 
+            break # ends check for even letters
+        else:
+            words = words[1:len(words)] + words[0:1]
+    return words
+words = input("Enter a word to convert into pig latin: ").split() # Asks user for input string to convert to pig latin
+words = [pigLatin(word) for word in words] # Converts each word into pig latin using the pigLatin method
+output_phrase = " ".join(words)
+print(output_phrase) # Finally outputs the pig latin phrase
